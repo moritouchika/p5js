1736c1656a257a5c7a9609136960ab79db058e62
moritou <sennka1038013@gmail.com>
2021-10-07 15:22:58 +0900

    r

--- projects/circle/sketch.js
+++ projects/circle/sketch.js
@@ -8,9 +8,9 @@ function draw() {
   // put drawing code here
   background(128, 128, 128);
   if(mouseIsPressed){
-    r = random(255);
-    g = random(255);
-    b = random(255);
+    r = 255;
+    g = 255;
+    b = 255;
     fill(r, g, b);
   r = 50;
   ellipse(mouseX, mouseY, r, r);
